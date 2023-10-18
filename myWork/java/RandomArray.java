60e114f0d93c1c7781c1a5f5e8f94a8e58d87935
Yaswanthkumar-piradhi <146201722+Yaswanthkumar-piradhi@users.noreply.github.com>
2023-10-18 22:37:40 +0530

    Add files via upload

---
+++ myWork/java/RandomArray.java
@@ -0,0 +1,19 @@
+public class RandomArray {
+    public static void shuffle(Object[] array) {
+        int noOfElements = array.length;
+        for (int i = 0; i < noOfElements; i++) {
+            int s = i + (int) (Math.random() * (noOfElements - i));
+            Object temp = array[s];
+            array[s] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {1, 2, 3, 4, 5, 6, 7};
+        RandomArray.shuffle(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
